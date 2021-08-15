0b3329aab9239d3b4063115a56848159654dd0fe
munokura <munokura@users.noreply.github.com>
2021-08-15 18:40:06 +0900

    悪夢追加

---
+++ js/plugins/MNKR_NonGoldWindow.js
@@ -0,0 +1,36 @@
+/*
+ * --------------------------------------------------
+ * MNKR_NonGoldWindow.js
+ *   Ver.1.0.1
+ * Copyright (c) 2020 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
+/*:
+ * @target MV
+ * @url https://raw.githubusercontent.com/munokura/MNKR-MV-plugins/master/MNKR_NonGoldWindow.js
+ * @plugindesc メインメニューで所持金ウィンドウを非表示にします。
+ * @author munokura
+ *
+ * @help
+ * メインメニューで所持金ウィンドウを非表示にします。
+ *
+ * プラグインコマンドや設定はありません。
+ *
+ *
+ * 利用規約:
+ *   MITライセンスです。
+ *   https://licenses.opensource.jp/MIT/MIT.html
+ *   作者に無断で改変、再配布が可能で、
+ *   利用形態（商用、18禁利用等）についても制限はありません。
+ */
+
+(function(){
+	'use strict';
+
+	Scene_Menu.prototype.createGoldWindow = function() {
+	};
+
+})();
